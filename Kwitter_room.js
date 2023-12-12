b4dfe72be6d054285e410178c52982fba25357d6
Izuku-Midori <125033419+Izuku-Midori@users.noreply.github.com>
2023-12-12 20:53:31 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,14 @@
+
+user_name = localStorage("user_name")
+function addRoom()
+{
+    room_name = document.getElementById("room_name").value;
+
+    firebase.database().ref("/").child(room_name).update({
+        purpose : "adding room name"
+    });
+
+    localStorage.setItem("room_name", room_name);
+
+    window.location = "kwitter_page.html";
+}
